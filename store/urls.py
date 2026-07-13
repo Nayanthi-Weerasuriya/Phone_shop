3719e04ba26610a1bb6b863279b61ea50ab4f230
Nayanthi Weerasuriya <nayanthiweerasuriya2003@gmail.com>
2026-07-13 16:33:39 +0530

    Modifications

--- store/urls.py
+++ store/urls.py
@@ -2,14 +2,16 @@
 
 from django.urls import path
 from django.contrib import admin
-
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('', views.storehome, name='storehome'),
     path('aboutus', views.aboutus, name='aboutus'),
     path('reviews', views.reviews, name='reviews'),
     path('admin/', admin.site.urls),
+    path('product/<int:product_id>', views.product, name='product')
+    
 
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,13 +1,38 @@
 """Views for the store app."""
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
-# Create your views here.
+
 def storehome(request):
-    return render(request, 'store/storehome.html', {'title': 'Store Home'})
+    products = Product.objects.all()
+    categories = Category.objects.all()
+
+    context = {
+        'products': products,
+        'categories': categories,
+    }
+
+    return render(request, 'store/storehome.html', context)
+
 
 def aboutus(request):
-    return render(request, 'store/aboutus.html', {'title': 'About Us'})
+    return render(request, 'store/aboutus.html', {
+        'title': 'About Us'
+    })
+
 
 def reviews(request):
-    return render(request, 'store/reviews.html', {'title': 'Reviews'})
+    return render(request, 'store/reviews.html', {
+        'title': 'Reviews'
+    })
+
+
+def product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'store/productinfo.html', context)
